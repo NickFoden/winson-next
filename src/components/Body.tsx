7f0927ace4795556c39843f93729839580cd3c98
Nick Foden <nickfoden@gmail.com>
2021-01-16 19:31:58 -0500

    setup meta tags and add assets

---
+++ src/components/Body.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const Body = ({ children }) => (
+  <>
+    <Head>
+      <title>Win Son</title>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content="Win Son" key="title" name="title" />
+      <meta
+        name="description"
+        content="Winson - taiwanese american restaurant and bakery in Brooklyn New York City"
+      />
+      <meta property="og:image" content="/winson-bear.jpg" />
+      <meta property="og:url" content="https://winsonbrooklyn.com/" />
+    </Head>
+    {children}
+  </>
+);
+
+export default Body;
