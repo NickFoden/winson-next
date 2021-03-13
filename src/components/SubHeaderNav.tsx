dd350c0164eeed16c43e1b0527072cf475aacd3c
Nick Foden <nickfoden@gmail.com>
2021-03-13 10:47:00 -0500

    add sub nav and use svgs

---
+++ src/components/SubHeaderNav.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const SubHeaderNav = () => {
+  const router = useRouter();
+
+  const menuLink = () => {
+    if (router?.pathname?.includes("restaurant")) {
+      return "/restaurant/menu";
+    } else {
+      return "/bakery/menu";
+    }
+  };
+  return (
+    <>
+      <nav>
+        <Link href={menuLink()}>
+          <a
+            className={
+              router && router.pathname.includes("menu")
+                ? "venue_active venue"
+                : "venue"
+            }
+          >
+            <img src="/svg/menu.svg" />
+          </a>
+        </Link>
+      </nav>
+      <style jsx>{`
+        img {
+          height: 1rem;
+        }
+        nav {
+          margin-top: 1rem;
+        }
+        .venue {
+          margin-right: 2rem;
+        }
+        .venue:hover {
+          opacity: 0.7;
+        }
+        .venue_active {
+          border-bottom: 3px solid black;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default SubHeaderNav;
