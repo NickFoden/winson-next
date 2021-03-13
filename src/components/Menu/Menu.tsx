2f89ec75c71a9724e7adbef808347e49beb96c0d
Nick Foden <nickfoden@gmail.com>
2021-03-13 10:13:48 -0500

    update site map and add sub menus

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,23 +4,32 @@ import { StyledMenu } from "./Menu.styled";
 
 const Menu = ({ open, ...props }) => {
   const isHidden = open ? true : false;
-  const tabIndex = isHidden ? 0 : -1;
 
   return (
     <>
       <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
         <Link href="/restaurant">
-          <a tabIndex={tabIndex}>Restaurant</a>
+          <a>Restaurant</a>
+        </Link>
+        <Link href="/restaurant/menu">
+          <a className="sub">menu</a>
         </Link>
         <Link href="/bakery">
-          <a tabIndex={tabIndex}>Bakery</a>
+          <a>Bakery</a>
+        </Link>
+        <Link href="/bakery/menu">
+          <a className="sub">menu</a>
         </Link>
       </StyledMenu>
       <style jsx>{`
         a {
           color: black;
           font-family: "Courier New", Courier, monospace;
+          padding: 1rem 0;
           text-decoration: none;
+        }
+        .sub {
+          font-size: 1.25rem;
         }
       `}</style>
     </>
